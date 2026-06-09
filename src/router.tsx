b4d018fd21f9c3f633de4d9a3d3d365d952a7525
Carla <carla@exemplo.com>
2026-06-09 12:53:26 -0300

    corrigir router.tsx - build funcionando

--- src/router.tsx
+++ src/router.tsx
@@ -1,51 +1,51 @@
-import { createRouter, createRootRoute, createRoute } from '@tanstack/react-router' 
-import App from './App' 
-import Home from './routes/Home' 
-import Turismo from './routes/Turismo' 
-import Hospedagem from './routes/Hospedagem' 
-import Negocios from './routes/Negocios' 
-import Clube from './routes/Clube' 
- 
-const rootRoute = createRootRoute({ 
-  component: App, 
-}) 
- 
-const homeRoute = createRoute({ 
-  getParentRoute: () =
-  path: '/', 
-  component: Home, 
-}) 
- 
-const turismoRoute = createRoute({ 
-  getParentRoute: () =
-  path: '/turismo', 
-  component: Turismo, 
-}) 
- 
-const hospedagemRoute = createRoute({ 
-  getParentRoute: () =
-  path: '/hospedagem', 
-  component: Hospedagem, 
-}) 
- 
-const negociosRoute = createRoute({ 
-  getParentRoute: () =
-  path: '/negocios', 
-  component: Negocios, 
-}) 
- 
-const clubeRoute = createRoute({ 
-  getParentRoute: () =
-  path: '/clube', 
-  component: Clube, 
-}) 
- 
-const routeTree = rootRoute.addChildren([ 
-  homeRoute, 
-  turismoRoute, 
-  hospedagemRoute, 
-  negociosRoute, 
-  clubeRoute, 
-]) 
- 
-export const router = createRouter({ routeTree }) 
+import { createRouter, createRootRoute, createRoute } from '@tanstack/react-router'
+import App from './App'
+import Home from './routes/Home'
+import Turismo from './routes/Turismo'
+import Hospedagem from './routes/Hospedagem'
+import Negocios from './routes/Negocios'
+import Clube from './routes/Clube'
+
+const rootRoute = createRootRoute({
+  component: App,
+})
+
+const homeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: Home,
+})
+
+const turismoRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/turismo',
+  component: Turismo,
+})
+
+const hospedagemRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/hospedagem',
+  component: Hospedagem,
+})
+
+const negociosRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/negocios',
+  component: Negocios,
+})
+
+const clubeRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/clube',
+  component: Clube,
+})
+
+const routeTree = rootRoute.addChildren([
+  homeRoute,
+  turismoRoute,
+  hospedagemRoute,
+  negociosRoute,
+  clubeRoute,
+])
+
+export const router = createRouter({ routeTree })
